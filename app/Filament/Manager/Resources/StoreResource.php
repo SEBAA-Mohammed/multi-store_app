da584c144699560cb90e69e76114cfe7ebc12431
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-04-20 14:17:08 +0100

    Store resource added in the Manager panel

---
+++ app/Filament/Manager/Resources/StoreResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Manager\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Store;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Manager\Resources\StoreResource\Pages;
+use App\Filament\Manager\Resources\StoreResource\Pages\EditStore;
+use App\Filament\Manager\Resources\StoreResource\Pages\ViewStore;
+use App\Filament\Manager\Resources\StoreResource\Pages\ListStores;
+use App\Filament\Manager\Resources\StoreResource\RelationManagers;
+use App\Filament\Manager\Resources\StoreResource\Pages\CreateStore;
+
+class StoreResource extends Resource
+{
+    protected static ?string $model = Store::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListStores::route('/'),
+            'create' => CreateStore::route('/create'),
+            'view' => ViewStore::route('/{record}'),
+            'edit' => EditStore::route('/{record}/edit'),
+        ];
+    }
+}
