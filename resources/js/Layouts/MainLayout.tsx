8c844447609cbd713cc4f6c1e4c78c4d70711c3d
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-04-21 16:38:04 +0100

    MainLayout component added

---
+++ resources/js/Layouts/MainLayout.tsx
@@ -0,0 +1,24 @@
+import { Head } from '@inertiajs/react';
+import { PropsWithChildren } from 'react';
+
+// import ApplicationLogo from '@/Components/ApplicationLogo';
+
+export default function MainLayout({ title, children }: PropsWithChildren<{ title?: string }>) {
+  return (
+    <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+      {/* <div>
+        <Link href="/">
+          <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+        </Link>
+      </div> */}
+
+      <Head>
+        <title>{title ? `${title}` : 'My Store'}</title>
+      </Head>
+
+      <main className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        {children}
+      </main>
+    </div>
+  );
+}
--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,7 +1,11 @@
+import MainLayout from '@/Layouts/MainLayout';
+
 export default function Index() {
   return (
-    <div className="">
-      <h1>Index</h1>
-    </div>
+    <MainLayout title="Home">
+      <div className="text-8xl">
+        <h1>Index</h1>
+      </div>
+    </MainLayout>
   );
 }
