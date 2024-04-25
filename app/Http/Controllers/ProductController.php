eff37e526e39abeb03e5c941dedb103009119e90
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-04-25 18:19:11 +0100

    Render suggested products component based on the same category of the current product in the individual product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,8 +14,18 @@ class ProductController extends Controller
      */
     public function __invoke(User $user, Store $store, Product $product)
     {
+        // Get the category ID of the current product
+        $categoryId = $product->category->id;
+
+        // Query suggested products that belong to the same category but exclude the current product
+        $suggestedProducts = $store->products()
+            ->where('category_id', $categoryId)
+            ->where('id', '!=', $product->id)
+            ->get();
+
         return inertia('Product', [
-            'product' => new ProductResource($product)
+            'product' => $product,
+            'suggestedProducts' => ProductResource::collection($suggestedProducts)
         ]);
 
         // return response()->json([
--- resources/js/Pages/Product.tsx
+++ resources/js/Pages/Product.tsx
@@ -1,9 +1,28 @@
+import { ProductList } from '@/Components/ProductList';
+import { Container } from '@/Components/ui/container';
 import { MainLayout } from '@/Layouts/MainLayout';
+import { Product as ProductType } from '@/types';
 
-export default function Product() {
+interface ProductProps {
+  product: ProductType;
+  suggestedProducts: ProductType[];
+}
+
+export default function Product({ product, suggestedProducts }: ProductProps) {
   return (
-    <div className="">
-      <h1>Product Page</h1>
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          {/* <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <Gallery images={product.images} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
+          </div> */}
+          <hr className="my-10" />
+          <ProductList title="Related Items" items={suggestedProducts} />
+        </div>
+      </Container>
     </div>
   );
 }
