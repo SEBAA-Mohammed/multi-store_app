db642e031ae92330c7ced413800d0f29b1296bfb
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-04-29 17:13:36 +0100

    Order details added to the order resource

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -5,11 +5,14 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
+use App\Models\Product;
 use Filament\Actions\CreateAction;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Repeater;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -35,17 +38,35 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('user_id')
-                    ->label('Brand')
-                    ->relationship('user', 'name')
-                    ->required(),
-                Forms\Components\Textarea::make('adresse_livraison')
-                    ->required(),
-                Forms\Components\Radio::make('is_paid')
-                    ->label('Paid ?')
-                    ->boolean()
+                Forms\Components\Section::make('Order informations')
+                    ->schema([
+                        Forms\Components\Select::make('user_id')
+                            ->label('User name')
+                            ->relationship('user', 'name')
+                            ->required(),
+                        Forms\Components\Textarea::make('adresse_livraison')
+                            ->required(),
+                        Forms\Components\Radio::make('is_paid')
+                            ->label('Paid ?')
+                            ->boolean()
+                    ]),
+
+                Forms\Components\Section::make('Order items')
+                    ->headerActions([
+                        Action::make('reset')
+                            ->modalHeading('Are you sure?')
+                            ->modalDescription('All existing items will be removed from the order.')
+                            ->requiresConfirmation()
+                            ->color('danger')
+                            ->action(fn (Forms\Set $set) => $set('items', [])),
+                    ])
+                    ->columnSpan('full') // Set the column span to full width
+                    ->schema([
+                        static::getItemsRepeater(),
+                    ]),
             ]);
     }
+
 
 
 
@@ -86,17 +107,81 @@ class OrderResource extends Resource
     }
 
 
-    // public static function infolist(Infolist $infolist): Infolist
-    // {
-    //     return $infolist
-    //         ->schema([
-    //             Infolists\Components\TextEntry::make('user.name'),
-    //             Infolists\Components\TextEntry::make('created_at'),
-    //             Infolists\Components\TextEntry::make('is_paid'),
-    //             Infolists\Components\TextEntry::make('adresse_livraison'),
-    //             Infolists\Components\TextEntry::make('status.name'),
-    //         ]);
-    // }
+
+    public static function getItemsRepeater(): Repeater
+    {
+        return Repeater::make('orderDetails')
+            ->relationship()
+            ->schema([
+                Forms\Components\Select::make('product_id')
+                    ->label('Product')
+                    ->options(Product::query()->pluck('designation', 'id'))
+                    ->required()
+                    ->reactive()
+                    ->afterStateUpdated(fn ($state, Forms\Set $set) => $set('prix_achat', Product::find($state)?->price ?? 0))
+                    ->distinct()
+                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                    ->columnSpan([
+                        'md' => 4,
+                    ])
+                    ->searchable(),
+
+                Forms\Components\TextInput::make('qte')
+                    ->label('Quantity')
+                    ->numeric()
+                    ->default(1)
+                    ->columnSpan([
+                        'md' => 2,
+                    ])
+                    ->required(),
+
+                Forms\Components\TextInput::make('prix_achat')
+                    ->label('Unit Price')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 2,
+                    ]),
+                Forms\Components\TextInput::make('tva_achat')
+                    ->label('TVA')
+                    ->disabled()
+                    ->dehydrated()
+                    ->numeric()
+                    ->required()
+                    ->columnSpan([
+                        'md' => 2,
+                    ]),
+            ])
+            ->extraItemActions([
+                Action::make('openProduct')
+                    ->tooltip('Open product')
+                    ->icon('heroicon-m-arrow-top-right-on-square')
+                    ->url(function (
+                        array $arguments,
+                        Repeater $component
+                    ): ?string {
+                        $itemData = $component->getRawItemState($arguments['item']);
+
+                        $product = Product::find($itemData['product_id']);
+
+                        if (!$product) {
+                            return null;
+                        }
+
+                        return ProductResource::getUrl('edit', ['record' => $product]);
+                    }, shouldOpenInNewTab: true)
+                    ->hidden(fn (array $arguments, Repeater $component): bool => blank($component->getRawItemState($arguments['item'])['product_id'])),
+            ])
+            ->orderColumn('sort')
+            ->defaultItems(1)
+            ->hiddenLabel()
+            ->columns([
+                'md' => 10,
+            ])
+            ->required();
+    }
 
 
     public static function getRelations(): array
