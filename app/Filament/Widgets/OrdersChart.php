5c394b8e17b8298fa248ee571c67f057845434d2
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-04-29 11:02:39 +0100

    Orders chart added , new library used its laravel-trend

---
+++ app/Filament/Widgets/OrdersChart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class OrdersChart extends ChartWidget
+{
+    protected static ?string $heading = 'Orders Chart';
+
+    protected static string $color = 'info';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Order::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Orders',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
