4581c20b3ea45b28008cde1aadbbc48f6057d603
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-01 17:52:02 +0100

    Feat: Cart components are rendered

---
+++ resources/js/Components/CartItem.tsx
@@ -0,0 +1,42 @@
+import { Currency, X } from 'lucide-react';
+
+import { IconButton } from '@/Components/ui/icon-button';
+import { Product } from '@/types';
+
+interface CartItemProps {
+  data: Product;
+}
+
+export function CartItem({ data }: CartItemProps) {
+  //   const cart = useCart();
+
+  const onRemove = () => {
+    // cart.removeItem(data.id);
+  };
+
+  return (
+    <li className="flex py-6 border-b">
+      <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
+        <img src={data.images[0].url} alt="" className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+        <div className="absolute z-10 right-0 top-0">
+          <IconButton onClick={onRemove} icon={<X size={15} />} />
+        </div>
+        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+          <div className="flex justify-between">
+            <p className=" text-lg font-semibold text-black">{data.designation}</p>
+          </div>
+
+          <div className="mt-1 flex text-sm">
+            {/* <p className="text-gray-500">{data.color.name}</p>
+            <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">{data.size.name}</p> */}
+            <p className="text-gray-500">{data.brand?.name}</p>
+            <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">{data.unit?.name}</p>
+          </div>
+          {/* <Currency value={data.price} /> */}
+        </div>
+      </div>
+    </li>
+  );
+}
--- resources/js/Pages/Cart.tsx
+++ resources/js/Pages/Cart.tsx
@@ -1,11 +1,30 @@
+import { CartItem } from '@/Components/CartItem';
+import { Summary } from '@/Components/Summary';
+import { Container } from '@/Components/ui/container';
 import { MainLayout } from '@/Layouts/MainLayout';
-import { Fragment } from 'react';
 
 export default function Cart() {
   return (
-    <Fragment>
-      <div className="space-y-10 pb-10">CART PAGE</div>
-    </Fragment>
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            {/* <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart.</p>
+              )}
+              <ul>
+                {cart.items.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </ul>
+            </div> */}
+            <Summary />
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
 
