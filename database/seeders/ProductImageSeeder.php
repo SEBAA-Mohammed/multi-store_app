6f38afe5370cd00eb29534502fd8e6e4d322d10a
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-05-03 00:52:34 +0100

    ...

--- database/seeders/ProductImageSeeder.php
+++ database/seeders/ProductImageSeeder.php
@@ -43,6 +43,15 @@ class ProductImageSeeder extends Seeder
             "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
             "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
             "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+            "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+            "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+            "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+            "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+            "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+            "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+            "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+            "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
 
         ];
 
