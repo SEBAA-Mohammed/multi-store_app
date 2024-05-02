51c65a5e3f86e4d5ce31e8b642ef04ad830a04cb
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-05-02 16:16:30 +0100

    Store seeder done for only store number 1

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,10 +14,12 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
+        $stores = Store::all();
+
         $categories = [
             ['name' => 'Vegetables', 'url' => 'https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-2foodgroups_vegetables_detailfeature.jpg?sfvrsn=226f1bc7_6'],
             ['name' => 'Fruits', 'url' => 'https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4'],
-            ['name' => 'Clothes', 'url' => 'https://www.learnenglish.com/wp-content/uploads/clothes-1.jpg.webp'],
+            ['name' => 'Clothes', 'url' => 'https://toronto.citynews.ca/wp-content/blogs.dir/sites/10/2020/05/21/rack-clothing-shopping-unsplash-1024x576.jpg'],
             ['name' => 'Shoes', 'url' => 'https://d1nymbkeomeoqg.cloudfront.net/photos/28/53/406866_25193_XXXL.webp'],
             ['name' => 'Electronics', 'url' => 'https://www.ept.ca/wp-content/uploads/2020/12/Screen-Shot-2020-12-10-at-10.48.19-AM.png'],
             ['name' => 'Books', 'url' => 'https://assets.teenvogue.com/photos/5e6bffbbdee1770008c6d9bd/16:9/w_1920,c_limit/GettyImages-577674005.jpg'],
@@ -28,10 +30,13 @@ class CategorySeeder extends Seeder
         ];
 
         foreach ($categories as $category) {
+            $storeId = $category['name'] === 'Clothes' || $category['name'] === 'Shoes' || $category['name'] === 'Sports Equipment' ? $stores->first()->id : $stores->random()->id;
+
+
             DB::table('categories')->insert([
                 'name' => $category['name'],
                 'url' => $category['url'],
-                'store_id' => Store::inRandomOrder()->first()->id
+                'store_id' => $storeId
             ]);
         }
     }
