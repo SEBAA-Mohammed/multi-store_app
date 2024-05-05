328583f8ec67652a774172936e1250a178bf3f70
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-05-05 14:21:26 +0100

    stores created chart added to manager

---
+++ app/Filament/Manager/Widgets/StoresChart.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Manager\Widgets;
+
+use App\Models\Store;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class StoresChart extends ChartWidget
+{
+    protected static ?string $heading = 'Stores Chart';
+
+    protected static string $color = 'primary';
+
+    protected static ?string $pollingInterval = '60s';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Store::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count('id');
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Number of Stores created',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            'scales' => [
+                'y' => [
+                    'beginAtZero' => true,
+                    'ticks' => [
+                        'stepSize' => 1, // Display only whole numbers
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
