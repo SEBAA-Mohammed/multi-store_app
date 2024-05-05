6df70d5bcd8381d81890b4868f84dc9ab17c9ddf
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-05-05 23:50:23 +0100

    just fixing page title

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-        Tailwind Starter Template - Landing Page Template: Tailwind Toolbox
+        Store Builder | Home
     </title>
     <meta name="description" content="Simple landind page" />
     <meta name="keywords" content="" />
