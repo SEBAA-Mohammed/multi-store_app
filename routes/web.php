61b3f6b1ff1f64fe2563a6adda6dddfdc46bb695
Mohammed-SEBAA <mohammedsebaa2610@gmail.com>
2024-05-05 20:12:55 +0100

    Landing page start up

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [LandingPageController::class, 'index'])->name('landing page');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
