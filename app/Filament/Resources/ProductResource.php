950bd88dc746b08e3fdb2aaca45aa99a2bcbab95
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-06 19:09:28 +0100

    Fix: Fix product images not loading in the frontend

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -13,6 +13,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
 class ProductResource extends Resource
 {
