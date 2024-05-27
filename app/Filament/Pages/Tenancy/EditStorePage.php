0b80a9ffa25c2f925dd4531661693f03f41c5e92
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-05-27 17:38:44 +0100

    store sittings configured

--- app/Filament/Pages/Tenancy/EditStorePage.php
+++ app/Filament/Pages/Tenancy/EditStorePage.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Pages\Tenancy;
 
 use Filament\Forms\Form;
+use Filament\Forms;
+
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Tenancy\EditTenantProfile;
 
@@ -10,7 +12,7 @@ class EditStorePage extends EditTenantProfile
 {
     public static function getLabel(): string
     {
-        return 'Store setting';
+        return 'Store settings';
     }
 
     public function form(Form $form): Form
@@ -20,6 +22,25 @@ class EditStorePage extends EditTenantProfile
                 TextInput::make('name')
                     ->required()
                     ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required(),
+                TextInput::make('tel')
+                    ->required(),
+                TextInput::make('adresse')
+                    ->required(),
+                Forms\Components\Select::make('store_category_id')
+                    ->label('Category')
+                    ->relationship('storeCategory', 'name')
+                    ->required(),
+                TextInput::make('header')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+                Forms\Components\FileUpload::make('logo_url')
+                    ->image(),
+                Forms\Components\FileUpload::make('billboard_url')
+                    ->image(),
             ]);
     }
 }
